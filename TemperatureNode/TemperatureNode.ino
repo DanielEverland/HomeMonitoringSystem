f2e17c6c77ce71fa4d7b6f57b3f4a4971e03cac6
KTLDTU <72263629+KTLDTU@users.noreply.github.com>
2023-04-13 16:51:09 +0200

    Add photoresistor to temperaturenode

--- TemperatureNode/TemperatureNode.ino
+++ TemperatureNode/TemperatureNode.ino
@@ -2,42 +2,42 @@
 #include <ESP8266WiFi.h>  //https://github.com/esp8266/Arduino/blob/master/libraries/ESP8266WiFi/src/ESP8266WiFi.h
 #include <DHT.h>
 
+
 //Constants
 #define UPDATE_TIME 1000
-#define DHTTYPE DHT11  // DHT 11
+#define DHTTYPE DHT11
 //Parameters
 String nom = "TemperatureNode";
 const char* ssid = "Galaxy A53";
 const char* password = "expeditious";
+
 //Variables
 unsigned long previousRequest = 0;
 //Objects
 WiFiClient host;
 IPAddress server(192, 168, 127, 85);
-float t;
-float h;
-float f;
-
 // Initialize DHT sensor.
 // From left to right facing the sensor:
 // S, VCC, GND
 const int DHTPin = D3;
 DHT dht(DHTPin, DHTTYPE);
+float t;
+float h;
+float f;
 
-// Temporary variables
-static char celsiusTemp[7];
-static char fahrenheitTemp[7];
-static char humidityTemp[7];
-
-
-void dhtSensorLoop();
+const int pResistor = A0;
+int lightValue;
+String lightDetected;
 
 void setup() {
+  pinMode(pResistor, INPUT);
+
   dht.begin();
   //Init Serial USB
   Serial.begin(115200);
   Serial.println(F("Initialize System"));
   //Init ESP8266 Wifi
+
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
@@ -49,17 +49,17 @@ void setup() {
 }
 
 void loop() {
-  dhtSensorLoop();
-  requestHost();
+  if ((millis() - previousRequest) > UPDATE_TIME) {
+    previousRequest = millis();
+    dhtSensorLoop();
+    lightSensorLoop();
+    requestHost();
+  }
 }
 
-void requestHost() { /* function requestMaster */
-  ////Request to host
-  if ((millis() - previousRequest) > UPDATE_TIME) {  // client connect to server every 500ms
-    previousRequest = millis();
-    if (host.connect(server, 81)) {  // Connection to the server
-      host.println("c[" + nom + "]c" + ": Current temperature is t[" + t + "]t Current humidity is h[" + h + "]h \r");
-    }
+void requestHost() {
+  if (host.connect(server, 81)) {
+    host.println("c[" + nom + "]c" + ": Current temperature is t[" + t + "]t Current humidity is h[" + h + "]h Current light level is: l[" + lightValue + "]l Light detected: ld[ " + lightDetected + " ]ld \r");
   }
 }
 
@@ -68,21 +68,25 @@ void dhtSensorLoop() {
   h = dht.readHumidity();
   // Read temperature as Celsius (the default)
   t = dht.readTemperature();
-  // Read temperature as Fahrenheit (isFahrenheit = true)
+  // Read temperature as Fahrenheit
   f = dht.readTemperature(true);
 
-  // Check if any reads failed and exit early (to try again).
-  if (isnan(h) || isnan(t) || isnan(f)) {
-    Serial.println("Failed to read from DHT sensor!");
-    strcpy(celsiusTemp, "Failed");
-    strcpy(fahrenheitTemp, "Failed");
-    strcpy(humidityTemp, "Failed");
+  Serial.print("Humidity: ");
+  Serial.print(h);
+  Serial.print(" %\t Temperature: ");
+  Serial.print(t);
+  Serial.print(" *C %\t Fahrenheit: ");
+  Serial.println(f);
+}
+
+void lightSensorLoop() {
+  lightValue = analogRead(pResistor);
+  if (lightValue > 100) {
+    lightDetected = "Light detected";
   } else {
-    Serial.print("Humidity: ");
-    Serial.print(h);
-    Serial.print(" %\t Temperature: ");
-    Serial.print(t);
-    Serial.print(" *C ");
-    Serial.println(f);
+    lightDetected = "No light detected";
   }
+  Serial.print(lightValue);
+  Serial.print(" | ");
+  Serial.println(lightDetected);
 }
