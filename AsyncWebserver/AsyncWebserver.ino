6826edaa07a394e9e9c06fe1816bcb6d3dae7c88
KTLDTU <72263629+KTLDTU@users.noreply.github.com>
2023-04-13 20:55:12 +0200

    Add comments, remove unused stuff, make server ip static

--- AsyncWebserver/AsyncWebserver.ino
+++ AsyncWebserver/AsyncWebserver.ino
@@ -1,111 +1,110 @@
- /*********
-  Rui Santos
-  Complete project details at https://randomnerdtutorials.com/esp8266-dht11dht22-temperature-and-humidity-web-server-with-arduino-ide/
-*********/
+/*
+  Initial ASyncWebServer setup credit: https://randomnerdtutorials.com/esp8266-dht11dht22-temperature-and-humidity-web-server-with-arduino-ide/
+*/
 
-// Import required libraries
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
-#include <Hash.h>
 #include <ESPAsyncTCP.h>
 #include <ESPAsyncWebServer.h>
+
+// Header file containing website HTML code
 #include "Website.generated.h"
 
-//void clientRequest();
-//String getClientSubstring();
+// Network credentials
+const char *ssid = "Galaxy A53";
+const char *password = "expeditious";
 
-// Replace with your network credentials
-const char* ssid = "Galaxy A53";
-const char* password = "expeditious";
-
-// current temperature & humidity, updated in loop()
+// Initialize variables to hold data from nodes
 float t = 0.0;
 float h = 0.0;
+float l = 0.0;
+String lightDetection = "";
 String motionDetection = "";
 
 // Create AsyncWebServer object on port 80
 AsyncWebServer server(80);
 
-// Generally, you should use "unsigned long" for variables that hold time
-// The value will quickly become too large for an int to store
-unsigned long previousMillis = 0;    // will store last time DHT was updated
-
-// WiFi server for connecting clients
+// WiFi server for connecting clients on port 81
 WiFiServer wifiServer(81);
-WiFiClient browser;
-IPAddress ip(192, 168, 158, 20);
-IPAddress gateway(192, 168, 1, 254);
+
+// Define static IP, gateway and subnet for better consistency
+
+// TODO: Check if this breaks client communication
+IPAddress ip(192, 168, 127, 20);
+IPAddress gateway(192, 168, 127, 254);
 IPAddress subnet(255, 255, 255, 0);
 
 // Names of nodes
 const String proximityName = "ProximityNode";
-const String tempHumNode = "TemperatureNode";
+const String tempHumLightNode = "TemHumLightNode";
 
-// Replaces placeholder with DHT values
-String processor(const String& var){
-  //Serial.println(var);
-  if(var == "TEMPERATURE"){
+// Function used to initialize values on website on initial load
+String processor(const String &var) {
+  if (var == "TEMPERATURE") {
     return String(t);
-  }
-  else if(var == "HUMIDITY"){
+  } else if (var == "HUMIDITY") {
     return String(h);
+  } else if (var == "MOTION") {
+    return motionDetection;
+  } else if (var == "LIGHT") {
+    return lightDetection;
   }
   return String();
 }
 
-void setup(){
+void setup() {
   // Serial port for debugging purposes
   Serial.begin(115200);
-  
+
   // Connect to Wi-Fi
+  WiFi.config(ip, gateway, subnet);
   WiFi.begin(ssid, password);
+
   Serial.println("Connecting to WiFi");
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
-    Serial.println(".");
+    Serial.print(".");
   }
-
+  Serial.println("Connection established");
   wifiServer.begin();
 
-  // Print ESP8266 Local IP Address
-  Serial.println(WiFi.localIP());
-
-  // Route for root / web page
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
+    // Use the processor() as a callback function when web root is loaded (to initialize data on load)
     request->send_P(200, "text/html", WEBSITE, processor);
   });
-  server.on("/temperature", HTTP_GET, [](AsyncWebServerRequest *request){
+  server.on("/temperature", HTTP_GET, [](AsyncWebServerRequest *request) {
     request->send_P(200, "text/plain", String(t).c_str());
   });
-  server.on("/humidity", HTTP_GET, [](AsyncWebServerRequest *request){
+  server.on("/humidity", HTTP_GET, [](AsyncWebServerRequest *request) {
     request->send_P(200, "text/plain", String(h).c_str());
   });
-  server.on("/motionDetection", HTTP_GET, [](AsyncWebServerRequest *request){
+  server.on("/motionDetection", HTTP_GET, [](AsyncWebServerRequest *request) {
     request->send_P(200, "text/plain", motionDetection.c_str());
   });
-  
-
+  server.on("/lightDetection", HTTP_GET, [](AsyncWebServerRequest *request) {
+    request->send_P(200, "text/plain", lightDetection.c_str());
+  });
   // Start server
   server.begin();
 }
- 
-void loop(){  
+
+void loop() {
   clientRequest();
 }
 
-void handleTemperatureInput(String request)
-{
+void handleTemperatureInput(String request) {
   t = strtof(getClientSubstring(request, "t").c_str(), nullptr);
   h = strtof(getClientSubstring(request, "h").c_str(), nullptr);
+  l = strtof(getClientSubstring(request, "l").c_str(), nullptr);
+  lightDetection = getClientSubstring(request, "ld");
 }
 
-void handleProximityInput(String request)
-{
+void handleProximityInput(String request) {
   motionDetection = getClientSubstring(request, "m");
 }
 
-void clientRequest() { /* function clientRequest */
-  ////Check if client connected
+void clientRequest() {
+  //Check if client connected
   WiFiClient client = wifiServer.available();
   client.setTimeout(50);
   if (client) {
@@ -114,21 +113,16 @@ void clientRequest() { /* function clientRequest */
       /*Serial.print(" ->");
       Serial.println(client.remoteIP());*/
 
-      String request = client.readStringUntil('\r');  //receives the message from the client
+      String request = client.readStringUntil('\r');  //receives the message from the client, use '\r' as String terminator
       Serial.print("Request: ");
       Serial.println(request);
 
       String clientName = getClientSubstring(request, "c");
-      if(clientName == tempHumNode)
-      {
+      if (clientName == tempHumLightNode) {
         handleTemperatureInput(request);
-      }
-      else if(clientName == proximityName)
-      {
+      } else if (clientName == proximityName) {
         handleProximityInput(request);
-      }
-      else
-      {
+      } else {
         Serial.println("ERROR: Could not identify client name " + clientName);
       }
     }
