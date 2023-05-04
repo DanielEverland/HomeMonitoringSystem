54cd5fdfa98839826cf23d44c02e527f6f7fe7e6
Daniel Everland <danieleverland@gmail.com>
2023-05-04 12:08:44 +0200

    TemperatureNode cleanup

--- TemperatureNode/TemperatureNode.ino
+++ TemperatureNode/TemperatureNode.ino
@@ -2,31 +2,45 @@
 #include <ESP8266WiFi.h>  //https://github.com/esp8266/Arduino/blob/master/libraries/ESP8266WiFi/src/ESP8266WiFi.h
 #include <DHT.h>
 
-
 //Constants
 #define UPDATE_TIME 1000
 #define DHTTYPE DHT11
-//Parameters
-String nom = "TemHumLightNode";
-const char* ssid = "Galaxy A53";
-const char* password = "expeditious";
 
-//Variables
+// Name of this peer on the network. Used in requests.
+const String peerName = "TemHumLightNode";
+
+// The name of the wifi network to connect to
+const char* wifiName = "Galaxy A53";
+
+// The password of the wifi network to connect to
+const char* wifiPassword = "expeditious";
+
+// The time the previous request was sent
 unsigned long previousRequest = 0;
-//Objects
+
+// The server this node connects to
 WiFiClient host;
+
+// The IP of the server to connect to
 IPAddress server(192, 168, 66, 85);
+
 // Initialize DHT sensor.
 // From left to right facing the sensor:
 // S, VCC, GND
 const int DHTPin = D3;
 DHT dht(DHTPin, DHTTYPE);
-float t;
-float h;
-float f;
 
-const int pResistor = A0;
+// Contains the measured temperature in celsius
+float temperature;
+
+// Contains the measured humidity
+float humidity;
+
+// Contains the light-level
 int lightValue;
+
+// The pin used for the light sensor
+const int pResistor = A0;
 
 void setup() {
   pinMode(pResistor, INPUT);
@@ -37,12 +51,12 @@ void setup() {
   Serial.println(F("Initialize System"));
   //Init ESP8266 Wifi
 
-  WiFi.begin(ssid, password);
+  WiFi.begin(wifiName, wifiPassword);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(F("."));
   }
-  Serial.print(nom);
+  Serial.print(peerName);
   Serial.print(F(" connected to Wifi! IP address : "));
   Serial.println(WiFi.localIP());  // Print the IP address
 }
@@ -59,24 +73,20 @@ void loop() {
 void requestHost() {
   if (host.connect(server, 81)) {
     // Remove light value if unused
-    host.println("c[" + nom + "]c" + ": Current temperature is t[" + t + "]t Current humidity is h[" + h + "]h Current light level is: l[" + lightValue + "]l \r");
+    host.println("c[" + peerName + "]c" + ": Current temperature is t[" + temperature + "]t Current humidity is h[" + humidity + "]h Current light level is: l[" + lightValue + "]l \r");
   }
 }
 
 void dhtSensorLoop() {
   // Read humidity
-  h = dht.readHumidity();
+  humidity = dht.readHumidity();
   // Read temperature as Celsius (the default)
-  t = dht.readTemperature();
-  // Read temperature as Fahrenheit
-  f = dht.readTemperature(true);
+  temperature = dht.readTemperature();
 
   Serial.print("Humidity: ");
-  Serial.print(h);
+  Serial.print(humidity);
   Serial.print(" %\t Temperature: ");
-  Serial.print(t);
-  Serial.print(" *C %\t Fahrenheit: ");
-  Serial.println(f);
+  Serial.print(temperature);
 }
 
 void lightSensorLoop() {
